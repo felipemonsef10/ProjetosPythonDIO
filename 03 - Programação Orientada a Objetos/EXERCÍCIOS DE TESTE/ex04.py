4be9feabbf16b91b75ec5b611888e0e099b1acd5
felipemonsef10 <felipemonsef10@gmail.com>
2023-04-28 21:40:04 -0300

    Update ex04.py

--- 03 - Programação Orientada a Objetos/EXERCÍCIOS DE TESTE/ex04.py
+++ 03 - Programação Orientada a Objetos/EXERCÍCIOS DE TESTE/ex04.py
@@ -13,8 +13,9 @@ class ContaBancaria:
     def __init__(self, nome, saldo):
         self.nome = nome
         self.saldo = saldo
-        self._saques_depositos = []
+        self._saques_depositos_transfer = []
         self._taxa_saque = 1.5
+        self._limite_valor_saque = 2000
 
     def mostrar_saldo(self):
         print('*' * 40)
@@ -24,43 +25,62 @@ class ContaBancaria:
         print('=' * 40)
         print(f'DEPÓSITO DE R${valor_deposito:.2f}')
         print('-' * 30)
-        self._saques_depositos.append(valor_deposito)
+        self._saques_depositos_transfer.append(valor_deposito)
         self.saldo += valor_deposito
         print('DEPÓSITO EFETUADO COM SUCESSO!')
 
     def sacar(self, valor_saque):
         print('=' * 40)
         print(f'SAQUE DE R${valor_saque:.2f}')
+        print(f'Taxa: R${valor_saque * self._taxa_saque / 100:.2f}\t(+{self._taxa_saque}%)')
         if (valor_saque * (1 + self._taxa_saque / 100)) > self.saldo:
             print('-' * 30)
             print('ERRO! VOCÊ NÃO POSSUI SALDO SUFICIENTE PARA O SAQUE!')
-        else:
+        elif (valor_saque * (1 + self._taxa_saque / 100)) < self.saldo:
             print('-' * 30)
-            print(f'Taxa: R${valor_saque * self._taxa_saque / 100:.2f}\t(+{self._taxa_saque}%)')
-            valor_saque *= 1 + (self._taxa_saque / 100)
-            self._saques_depositos.append(-valor_saque)
-            self.saldo -= valor_saque
-            print('SAQUE EFETUADO COM SUCESSO!')
+            if self._limite_valor_saque == 0:
+                print('ERRO! O VALOR LIMITE DE SAQUE FOI ATINGIDO!')
+                print(f'LIMITE DISPONÍVEL: R${self._limite_valor_saque:.2f}')
+            elif valor_saque * (1 + (self._taxa_saque / 100)) > self._limite_valor_saque:
+                print('ERRO! O VALOR DE SAQUE MAIS A TAXA ULTRAPASSA O VALOR DE LIMITE!')
+                print(f'LIMITE DISPONÍVEL: R${self._limite_valor_saque:.2f}')
+            else:
+                valor_saque *= 1 + (self._taxa_saque / 100)
+                self._saques_depositos_transfer.append(-valor_saque)
+                self.saldo -= valor_saque
+                self._limite_valor_saque -= valor_saque
+                print('SAQUE EFETUADO COM SUCESSO!')
+                print(f'LIMITE DISPONÍVEL: R${self._limite_valor_saque:.2f}')
 
     def visualizar_extrato(self):
         print('*' * 40)
         print('EXTRATO DA CONTA:')
         print('-' * 30)
-        for saque_deposito in self._saques_depositos:
-            if saque_deposito < 0:
-                saque_deposito *= -1
-                print(f'SAQUE: \t\tR${saque_deposito:.2f}  \t(+1.5%)')
+        for saque_deposito_transf in self._saques_depositos_transfer:
+            if saque_deposito_transf is str:
+                saque_deposito_transf.replace('t', '')
+                print(f'TRASFERÊNCIA: {float(saque_deposito_transf):.2f}')
             else:
-                print(f'DEPÓSITO: \tR${saque_deposito:.2f}')
-        print(f'SALDO: \t\tR${self.saldo:.2f}')
+                if saque_deposito_transf < 0:
+                    saque_deposito_transf *= -1
+                    print(f'SAQUE: \t\t\t\t\tR${saque_deposito_transf:.2f}  \t(+1.5%)')
+                else:
+                    print(f'DEPÓSITO: \t\t\t\tR${saque_deposito_transf:.2f}')
+        print(f'\nSALDO: \t\t\t\t\tR${self.saldo:.2f}')
+        print(f'LIMITE DISPONÍVEL: \t\tR${self._limite_valor_saque:.2f}')
         print('*' * 40)
 
+    def fazer_transferencia(self, valor_transferencia, conta_destino):
+        print(f'VALOR DE TRANSFERÊNCIA: R${valor_transferencia:.2f}')
+        if valor_transferencia > self.saldo:
+            print('ERRO! SALDO INSUFICIENTE PARA COMPLETAR A TRANSAÇÃO!')
+        else:
+            print('TRANSFERÊNCIA REALIZADA COM SUCESSO!')
+            self.saldo -= valor_transferencia
+            conta_destino.depositar(valor_transferencia)
+            self._saques_depositos_transfer.append('t' + str(valor_transferencia))
 
-conta1 = ContaBancaria('Felipe', 200)
-conta1.sacar(200)
-conta1.depositar(10)
-conta1.sacar(200)
-conta1.depositar(500)
-conta1.sacar(507)
 
-conta1.visualizar_extrato()
+conta1 = ContaBancaria('Felipe', 2000)
+conta2 = ContaBancaria('João', 0)
+conta1.fazer_transferencia(500, conta2)
